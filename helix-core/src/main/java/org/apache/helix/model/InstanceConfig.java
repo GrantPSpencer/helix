f11120738dc55b1af76995860a5bbbd61855887f
Grant Palau Spencer <gspencer@linkedin.com>
2024-07-10 15:23:02 -0700

    todos

--- helix-core/src/main/java/org/apache/helix/model/InstanceConfig.java
+++ helix-core/src/main/java/org/apache/helix/model/InstanceConfig.java
@@ -680,6 +680,8 @@ public class InstanceConfig extends HelixProperty {
         getInstanceOperation().getOperation());
   }
 
+  // gspencer TODO:
+
   /**
   * Check if this instance is enabled for a given partition
   * This API is deprecated, and will be removed in next major release.
@@ -703,6 +705,7 @@ public class InstanceConfig extends HelixProperty {
    * @param partition the partition name to check
    * @return true if the instance is enabled for the partition, false otherwise
    */
+  @Deprecated
   public boolean getInstanceEnabledForPartition(String resource, String partition) {
     // TODO: Remove this old partition list check once old get API removed.
     List<String> oldDisabledPartition =
@@ -787,6 +790,8 @@ public class InstanceConfig extends HelixProperty {
     if (disabledPartitionsRawMap == null) {
       return Collections.emptyMap();
     }
+
+    // if all resource key, then thats the map
 
     Map<String, List<String>> disabledPartitionsMap = new HashMap<String, List<String>>();
     for (String resourceName : disabledPartitionsRawMap.keySet()) {
